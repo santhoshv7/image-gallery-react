bf2353e756a1f8520772b3ecc62eca00f5b612a7
santhoshv7 <fshsanthosh@gmail.com>
2024-10-28 22:57:11 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,76 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import black from "./assets/black-car.png"
+import blue from "./assets/blue-car.png"
+import red from "./assets/red-car.png"
+import white from "./assets/white-car.png"
+import yellow from "./assets/yellow-car.png"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function Heading(){
+  return(<div className = "heading">
+    <h1>Image Gallery</h1>
+  </div>)
+}
+
+function Gallery(props){
+  return(<div className="image-card">
+    <img src={props.image} alt= "car_name"></img>
+    <p>{props.car_name}</p>
+  </div>)
+}
+
+const details = [
+  {image: black,
+    name: "Aston Martin"
+  },
+  {
+    image: red,
+    name: "Lamborgini"
+  },
+  {
+    image: yellow,
+    name: "Ferrari"
+  },
+  {
+    image: white,
+    name: "Benz"
+  },
+  {
+    image: yellow,
+    name: "Audi"
+  },
+  {
+    image: black,
+    name: "Volvo"
+  },
+  {
+    image: blue,
+    name: "Jaguar"
+  },
+  {
+    image: red,
+    name: "Volkswagen"
+  },
+
+]
+
+
+
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+  <Heading/>
+  <div className="container">
+    {details.map((item) =>(
+      <Gallery image = {item.image} car_name = {item.name} ></Gallery>))}
+    
+
+  </div>
+
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
